33ea51ab0b8964b9e7e243f8fe42906f6b0cf2b2
slagos <slagos@defontana.com>
2026-02-24 23:00:25 -0300

    fast commit

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import type { BalanceResponse } from "./types";
 
 export async function balanceTeams(vanityUrls: string[]): Promise<BalanceResponse> {
-  const res = await fetch("/balance", {
+  const res = await fetch("https://8wq7aoimmk.execute-api.us-east-1.amazonaws.com/prod/balance", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ vanity_urls: vanityUrls }),
